dc1ccb72b4ebff715a1ba8eaa0d5a3deca4570f9
zack-murray <61975667+zack-murray@users.noreply.github.com>
2020-07-30 10:16:39 -0400

    Making progress

--- KMeans2.py
+++ KMeans2.py
@@ -8,11 +8,17 @@ class Kmeans:
         if self.seed is not None:
             np.random.seed(self.seed)
         self.max_iter = max_iter
+        
+    def initialize_centroids(self, data):
 
-    def initialize_centroids(self, data): 
         initial_centroids = np.random.permutation(data.shape[0])[:self.n_clusters]
         self.centroids = data[initial_centroids]
         return self.centroids
+    
+    # def get_og_centroids(self, data):
+
+    #     self.og_centroids = self.centroids
+    #     return self.og_centroids
 
     def assign_clusters(self, data):
         if data.ndim == 1:
@@ -20,6 +26,7 @@ class Kmeans:
 
         dist_to_centroid = distance.cdist(data, self.centroids, 'euclidean')
         self.cluster_labels = np.argmin(dist_to_centroid, axis=1)
+
         return self.cluster_labels
 
     def update_centroids(self, data):
@@ -28,12 +35,19 @@ class Kmeans:
 
     def fit(self, data):
         self.centroids = self.initialize_centroids(data)
+        # self.og_centroids = self.get_og_centroids(data)
         for iter in range(self.max_iter):
             self.cluster_labels = self.assign_clusters(data)
-            self.centroids == self.update_centroids(data)
-            if iter % 100 == 0:
+            self.centroids = self.update_centroids(data)
+            distances = distance.cdist(self.initialize_centroids(data), self.centroids, 'euclidean')
+            if sum(distances) == 0:
+                break
+            # if iter % 100 == 0:
+            if iter % 1 == 0:
                 print("Iterations run: %d" %iter)
-            return self
+        print("Iterations complete!")
+
+        return self
 
     def predict(self, data):
         return self.assign_clusters(data)
